b8d29a8b5091056e92d51ff2c990fa6d85e6ebf2
sssafia <chmiti.safia@gmail.com>
2023-10-15 15:11:42 +0200

    Create feedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,13 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Safia CHMITI on 15/10/2023.
+//
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
